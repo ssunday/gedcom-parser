31713237615abbaf014d6d1b6b3733c07764f586
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-10-18 10:53:38 -0500

    [SCRIPT] Harden name parsing and strip slashes

--- gedcom_to_csv.rb
+++ gedcom_to_csv.rb
@@ -2,18 +2,23 @@ require 'csv'
 
 HEADERS = %w[Name Sex].freeze
 
-NAME = '1 NAME'.freeze
-SEX = '1 SEX'.freeze
+NAME = '1 NAME '.freeze
+SEX = '1 SEX '.freeze
 
 def parse_row_value(line, deliminator)
-  line.split(deliminator).last.strip
+  line
+    .split(deliminator)
+    .last
+    .strip
+    .gsub('/', '')
 end
 
 def add_to_csv?(line, new_row)
   return false if new_row.empty?
   return true if line.include?('0 TRLR')
+  return false unless line.include?(' INDI')
 
-  line.include?('0 @I') && line.include?('INDI')
+  line.include?('0 @I')
 end
 
 CSV.open('./output.csv', 'w+') do |csv|
